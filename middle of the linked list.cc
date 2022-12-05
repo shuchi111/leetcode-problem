8b7b587c1122085c75b052a8bab3fde742b81d5f
Shuchi Shukla <107612618+shuchi111@users.noreply.github.com>
2022-12-05 05:54:03 +0530

    leetcode

---
+++ middle of the linked list.cc
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slowPtr = head;
+        ListNode* fastPtr = head;
+        
+        while(fastPtr->next != nullptr)
+        {
+            slowPtr = slowPtr->next;
+            
+            if(fastPtr->next->next != nullptr)
+            {
+                fastPtr = fastPtr->next->next;
+            }
+            else
+            {
+                fastPtr = fastPtr->next;
+            }
+        }
+        
+        return slowPtr;
+    }
+};
